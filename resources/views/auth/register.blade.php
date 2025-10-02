66243659f9c872dd0c31fed06bb3fd4b524b14b4
IsumiKalpage <isumikalpage@gmail.com>
2025-10-03 02:46:42 +0530

    UI modification

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -102,7 +102,7 @@
     {{-- Glass CSS --}}
     <style>
         .glass-card {
-            background: rgba(255, 255, 255, 0.2);
+            background: rgba(255, 255, 255, 0.1);
             border-radius: 20px;
             border: 1px solid rgba(255, 255, 255, 0.3);
             backdrop-filter: blur(25px) saturate(160%);
